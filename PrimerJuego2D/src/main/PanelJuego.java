73cc446570b732e92b81dfd5b05fca6230966179
Mauricio Fabian Farro Izquierdo <mauriciofarroizquierdo@gmail.com>
2025-12-23 00:34:18 -0500

    test git2

--- PrimerJuego2D/src/main/PanelJuego.java
+++ PrimerJuego2D/src/main/PanelJuego.java
@@ -20,7 +20,8 @@ import tiles.TileManager;
  */
 @SuppressWarnings("serial")
 public class PanelJuego extends JPanel implements Runnable {
-
+	int prueba ;
+	int prueba2;
 	final int OriginalTile = 32; // cambie el juego de 16x16 a 32x32
 	final int scale = 2;
 
